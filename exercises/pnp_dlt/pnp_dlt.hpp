4cd47a16797fea1a97e932bd0c67dd278c88c03a
Yousof <yousofebneddin@yahoo.com>
2022-06-06 21:41:27 -0700

    documentation

--- exercises/pnp_dlt/pnp_dlt.hpp
+++ exercises/pnp_dlt/pnp_dlt.hpp
@@ -13,7 +13,7 @@ read_detected_corners(
  * 
  * @param detected_corners corners coordinates in pixels and in homogenous system,
  * Matrix is in shape of (3 X n), n is number of corners.
- * @param points_in_W points in world coordinate.
+ * @param points_in_W points in world coordinate in shape of (3 x n).
  * @param K intrinsic camera matrix
  * @return Eigen::MatrixXd, the estimated pose of the camera in world coordinate. (4 x 4)
  */
